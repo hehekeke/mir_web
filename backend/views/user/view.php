2765751b78042c60ee9dd1d9d8eccc2a1e999130
wong <wonguohui@gmail.com>
2016-01-06 23:29:10 +0800

    view 页面修改

--- backend/views/user/view.php
+++ backend/views/user/view.php
@@ -6,40 +6,38 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model backend\models\MirUser */
 
-$this->title = $model->user_id;
+$this->title = '会员信息';
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Mir Users'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="mir-user-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->user_id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->user_id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'user_id',
-            'user_name',
-            'user_pwd',
-            'user_class',
-            'user_tel',
-            'user_email:email',
-            'user_chk',
-            'user_date',
-            'user_place',
-            'user_xm',
-            'user_com',
-        ],
-    ]) ?>
-
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="row">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            'user_id',
+                            'user_name',
+                            'user_pwd',
+                            'user_class',
+                            'user_tel',
+                            'user_email:email',
+                            'user_chk',
+                            'user_date',
+                            'user_place',
+                            'user_xm',
+                            'user_com',
+                        ],
+                    ]) ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
